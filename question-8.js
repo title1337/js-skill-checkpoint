857454274df5e02d0bc6a71764690e14fc05a2bf
ttestmaill01@gmail.com <ttestmaill01@gmail.com>
2026-04-27 06:59:36 +0700

    Fix Question-8

--- question-8.js
+++ question-8.js
@@ -6,8 +6,10 @@ async function fetchUserList() {
   const newUserList = data.map((user) => user.name);
   return newUserList;
 }
-const result = await fetchUserList();
-console.log(result);
-
+async function main() {
+  const result = await fetchUserList();
+  console.log(result);
+}
+main();
 // ใช้ fetch เพื่อดึงข้อมูลจาก API และใช้ .json() เพื่อแปลงข้อมูลเป็น JavaScript Object
 // จากนั้นใช้ .map() เพื่อสร้าง Array ใหม่ที่มีเฉพาะชื่อของผู้ใช้ (name) โดยการเข้าถึง property name ของแต่ละ user ใน data
