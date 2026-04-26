2608f1de3bc767d9e16b8e64c1b8b07ee4b55edc
Tle <title1337@gmail.com>
2026-04-26 09:32:08 +0700

    Feat Question8

--- question-8.js
+++ question-8.js
@@ -1,3 +1,13 @@
 // Question #8: Fetching User List from Server
 
-// เริ่มเขียนโค้ดตรงนี้
+async function fetchUserList() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+  const newUserList = data.map((user) => user.name);
+  return newUserList;
+}
+const result = await fetchUserList();
+console.log(result);
+
+// ใช้ fetch เพื่อดึงข้อมูลจาก API และใช้ .json() เพื่อแปลงข้อมูลเป็น JavaScript Object
+// จากนั้นใช้ .map() เพื่อสร้าง Array ใหม่ที่มีเฉพาะชื่อของผู้ใช้ (name) โดยการเข้าถึง property name ของแต่ละ user ใน data
