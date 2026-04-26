6dd6c7115df43120a931600623dfc17e9a72ccc5
Tle <title1337@gmail.com>
2026-04-26 08:40:54 +0700

    Feat Question2

--- question-2.js
+++ question-2.js
@@ -1,10 +1,13 @@
 // Question #2: User Task List
 
 const userTaskList = [
-  { id: 1, task: "Buy groceries" },
-  { id: 2, task: "Finish homework" },
-  { id: 3, task: "Call mom" },
-  { id: 4, task: "Wash dishes" },
+  { id: 1, task: 'Buy groceries' },
+  { id: 2, task: 'Finish homework' },
+  { id: 3, task: 'Call mom' },
+  { id: 4, task: 'Wash dishes' },
 ];
 
-// เริ่มเขียนโค้ดตรงนี้
+userTaskList.push({ id: 5, task: 'Walk the dog' }); // Add a new task to the list
+userTaskList[3].task = 'Go to the gym'; // Update the task description
+userTaskList.pop;
+console.log(userTaskList[3]);
