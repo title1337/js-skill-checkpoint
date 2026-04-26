e251730ade43a3b3606c92af15168d190a1c5161
Tle <title1337@gmail.com>
2026-04-26 08:47:40 +0700

    Feat Question3

--- question-3.js
+++ question-3.js
@@ -1,18 +1,18 @@
 // Question 3: User Authentication
 
 function authenticateUser(username, password) {
-  const validUsername = "user123";
-  const validPassword = "securepass";
-
-  // เริ่มเขียนโค้ดตรงนี้
+  const validUsername = 'user123';
+  const validPassword = 'securepass';
+  return username === validUsername && password === validPassword
+    ? 'Login successful.'
+    : 'Login failed. Please check your username and password.';
 }
 
-// ตัวอย่างการใช้งาน
-const username1 = "user123";
-const password1 = "securepass";
+const username1 = 'user123';
+const password1 = 'securepass';
 
-const username2 = "guest";
-const password2 = "password123";
+const username2 = 'guest';
+const password2 = 'password123';
 
 console.log(authenticateUser(username1, password1)); // Login successful.
 console.log(authenticateUser(username2, password2)); // Login failed. Please check your username and password.
