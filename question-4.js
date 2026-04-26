05be03ad23dadea47fe75cfc31a8a2ae57bada71
Tle <title1337@gmail.com>
2026-04-26 08:54:27 +0700

    Feat Question4

--- question-4.js
+++ question-4.js
@@ -1,7 +1,13 @@
 // Question 4: Shipping Cost Calculator
 
-function calculateShippingCost() {
-  // เริ่มเขียนโค้ดที่นี่
+function calculateShippingCost(orderTotal) {
+  if (orderTotal >= 4000) {
+    return 'Shipping is free.';
+  } else if (orderTotal >= 2000) {
+    return 'Shipping cost is 250 Baht.';
+  } else {
+    return 'Shipping cost is 500 Baht.';
+  }
 }
 
 // ตัวอย่างการใช้งาน
