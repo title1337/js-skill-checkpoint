c4f001f93ec90b52a27ff92b0d4c0cba941ea668
Tle <title1337@gmail.com>
2026-04-26 09:11:14 +0700

    Feat Question6

--- question-6.js
+++ question-6.js
@@ -1,14 +1,15 @@
 // Question #6: Calculating Total Price in User's Cart
 
 function filterInStockProducts(products) {
-  // เริ่มเขียนโค้ดตรงนี้
+  return products.filter((product) => product.quantity > 0);
 }
-
+// ใช้ filter เพื่อกรองเฉพาะสินค้าที่มี quantity มากกว่า 0
+// (product คือparameter ที่แทนแต่ละ Object ใน Array products ขณะที่ .filter() วน Loop))
 // ตัวอย่างการใช้งาน
 const products = [
-  { name: "apple", quantity: 2 },
-  { name: "banana", quantity: 0 },
-  { name: "orange", quantity: 5 },
+  { name: 'apple', quantity: 2 },
+  { name: 'banana', quantity: 0 },
+  { name: 'orange', quantity: 5 },
 ];
 
 console.log(filterInStockProducts(products));
