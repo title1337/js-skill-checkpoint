8f0447934a2010a4515dabecbeefd6034691f62e
Tle <title1337@gmail.com>
2026-04-26 09:17:27 +0700

    Feat Question7

--- question-7.js
+++ question-7.js
@@ -1,13 +1,24 @@
 // Question #7: Array Merging
 
 const firstProductList = [
-  { id: 1, name: "Laptop", price: 1000 },
-  { id: 2, name: "Smartphone", price: 700 },
+  { id: 1, name: 'Laptop', price: 1000 },
+  { id: 2, name: 'Smartphone', price: 700 },
 ];
 
 const secondProductList = [
-  { id: 3, name: "Tablet", price: 500 },
-  { id: 4, name: "Headphones", price: 150 },
+  { id: 3, name: 'Tablet', price: 500 },
+  { id: 4, name: 'Headphones', price: 150 },
 ];
 
-// เริ่มเขียนโค้ดตรงนี้
+function mergeProductLists(list1, list2) {
+  return [...list1, ...list2];
+}
+// ใช้ Spread Operator (...) เพื่อรวม Array list1 และ list2 เข้าด้วยกัน
+// ตัวอย่างการใช้งาน
+const mergedProductList = mergeProductLists(
+  firstProductList,
+  secondProductList,
+);
+console.log(mergedProductList);
+
+// อาจจะใช้ for...of + .push() ได้???
